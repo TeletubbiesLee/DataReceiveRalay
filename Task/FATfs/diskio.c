660a4df798a75c5a4fc5df8f9d67cbffd0525165
lilei <lilei@sojoline.com>
2019-10-18 14:27:31 +0800

    修改FAT文件系统调用Flash驱动的函数，还未进行测试

--- Task/FATfs/diskio.c
+++ Task/FATfs/diskio.c
@@ -9,11 +9,18 @@
 
 #include "ff.h"			/* Obtains integer types */
 #include "diskio.h"		/* Declarations of disk functions */
+#include "w25qxx.h"
 
 /* Definitions of physical drive number for each drive */
-#define DEV_RAM		0	/* Example: Map Ramdisk to physical drive 0 */
-#define DEV_MMC		1	/* Example: Map MMC/SD card to physical drive 1 */
-#define DEV_USB		2	/* Example: Map USB MSD to physical drive 2 */
+#define SD_CARD	 	0  			//SD卡,卷标为0
+#define EX_FLASH 	1			//外部spi flash,卷标为1
+#define EX_NAND  	2			//外部nand flash,卷标为2
+
+//对于W25Q256
+//前25M字节给fatfs用,25M字节后,用于存放字库,字库占用6.01M.	剩余部分,给客户自己用	 
+#define FLASH_SECTOR_SIZE 	512	
+#define FLASH_SECTOR_COUNT 	1024*25*2	//W25Q256,前25M字节给FATFS占用	
+#define FLASH_BLOCK_SIZE   	8     		//每个BLOCK有8个扇区	
 
 
 /*-----------------------------------------------------------------------*/
@@ -25,25 +32,27 @@ DSTATUS disk_status (
 )
 {
 	DSTATUS stat;
-	int result;
+	//int result;
 
 	switch (pdrv) {
-	case DEV_RAM :
-		result = RAM_disk_status();
+	case SD_CARD :
+		//result = RAM_disk_status();
 
 		// translate the reslut code here
 
 		return stat;
 
-	case DEV_MMC :
-		result = MMC_disk_status();
+	case EX_FLASH :
+		//result = MMC_disk_status();
 
 		// translate the reslut code here
 
+		stat = RES_OK;
+		
 		return stat;
 
-	case DEV_USB :
-		result = USB_disk_status();
+	case EX_NAND :
+		//result = USB_disk_status();
 
 		// translate the reslut code here
 
@@ -66,22 +75,24 @@ DSTATUS disk_initialize (
 	int result;
 
 	switch (pdrv) {
-	case DEV_RAM :
-		result = RAM_disk_initialize();
+	case SD_CARD :
+		//result = RAM_disk_initialize();
 
 		// translate the reslut code here
 
 		return stat;
 
-	case DEV_MMC :
-		result = MMC_disk_initialize();
+	case EX_FLASH :
+		//result = MMC_disk_initialize();
 
 		// translate the reslut code here
 
+		W25QXX_Init();  //W25QXX初始化
+		stat = RES_OK;
 		return stat;
 
-	case DEV_USB :
-		result = USB_disk_initialize();
+	case EX_NAND :
+		//result = USB_disk_initialize();
 
 		// translate the reslut code here
 
@@ -107,28 +118,35 @@ DRESULT disk_read (
 	int result;
 
 	switch (pdrv) {
-	case DEV_RAM :
+	case SD_CARD :
 		// translate the arguments here
 
-		result = RAM_disk_read(buff, sector, count);
+		//result = RAM_disk_read(buff, sector, count);
 
 		// translate the reslut code here
 
 		return res;
 
-	case DEV_MMC :
+	case EX_FLASH :
 		// translate the arguments here
 
-		result = MMC_disk_read(buff, sector, count);
+		//result = MMC_disk_read(buff, sector, count);
 
 		// translate the reslut code here
 
+		for(;count>0;count--)
+		{
+			W25QXX_Read(buff,sector*FLASH_SECTOR_SIZE,FLASH_SECTOR_SIZE);
+			sector++;
+			buff+=FLASH_SECTOR_SIZE;
+		}
+		res = RES_OK;
 		return res;
 
-	case DEV_USB :
+	case EX_NAND :
 		// translate the arguments here
 
-		result = USB_disk_read(buff, sector, count);
+		//result = USB_disk_read(buff, sector, count);
 
 		// translate the reslut code here
 
@@ -157,28 +175,35 @@ DRESULT disk_write (
 	int result;
 
 	switch (pdrv) {
-	case DEV_RAM :
+	case SD_CARD :
 		// translate the arguments here
 
-		result = RAM_disk_write(buff, sector, count);
+		//result = RAM_disk_write(buff, sector, count);
 
 		// translate the reslut code here
 
 		return res;
 
-	case DEV_MMC :
+	case EX_FLASH :
 		// translate the arguments here
 
-		result = MMC_disk_write(buff, sector, count);
+		//result = MMC_disk_write(buff, sector, count);
 
 		// translate the reslut code here
 
+		for(;count>0;count--)
+		{										    
+			W25QXX_Write((uint8_t*)buff,sector*FLASH_SECTOR_SIZE,FLASH_SECTOR_SIZE);
+			sector++;
+			buff+=FLASH_SECTOR_SIZE;
+		}
+		res = RES_OK;
 		return res;
 
-	case DEV_USB :
+	case EX_NAND :
 		// translate the arguments here
 
-		result = USB_disk_write(buff, sector, count);
+		//result = USB_disk_write(buff, sector, count);
 
 		// translate the reslut code here
 
@@ -205,19 +230,41 @@ DRESULT disk_ioctl (
 	int result;
 
 	switch (pdrv) {
-	case DEV_RAM :
+	case SD_CARD :
 
 		// Process of the command for the RAM drive
 
 		return res;
 
-	case DEV_MMC :
+	case EX_FLASH :
 
 		// Process of the command for the MMC/SD card
+	
+		switch(cmd)
+	    {
+		    case CTRL_SYNC:
+				res = RES_OK; 
+		        break;	 
+		    case GET_SECTOR_SIZE:
+		        *(WORD*)buff = FLASH_SECTOR_SIZE;
+		        res = RES_OK;
+		        break;	 
+		    case GET_BLOCK_SIZE:
+		        *(WORD*)buff = FLASH_BLOCK_SIZE;
+		        res = RES_OK;
+		        break;	 
+		    case GET_SECTOR_COUNT:
+		        *(DWORD*)buff = FLASH_SECTOR_COUNT;
+		        res = RES_OK;
+		        break;
+		    default:
+		        res = RES_PARERR;
+		        break;
+	    }
 
 		return res;
 
-	case DEV_USB :
+	case EX_NAND :
 
 		// Process of the command the USB drive
 
@@ -227,3 +274,12 @@ DRESULT disk_ioctl (
 	return RES_PARERR;
 }
 
+
+//获得时间
+//User defined function to give a current time to fatfs module      */
+//31-25: Year(0-127 org.1980), 24-21: Month(1-12), 20-16: Day(1-31) */                                                                                                                                                                                                                                          
+//15-11: Hour(0-23), 10-5: Minute(0-59), 4-0: Second(0-29 *2) */                                                                                                                                                                                                                                                
+DWORD get_fattime (void)
+{				 
+	return 0;
+}
